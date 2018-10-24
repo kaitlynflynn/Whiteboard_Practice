a1883e63162459025a9b4aa9a865df07ece55bcf
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-24 13:32:11 -0700

    Added some console log tests

--- RockPaperScissors/RockPaperScissors.js
+++ RockPaperScissors/RockPaperScissors.js
@@ -30,3 +30,16 @@ function rockPaperScissors(n) {
     findOutcome(n, []);
     return outcomes;
 }
+
+// Here are some console.log tests
+const rv1 = rockPaperScissors(2);
+console.log(rv1); // should print [[rock, rock], [rock, paper], [rock, scissors], [paper, rock], [paper, paper], [paper, scissors], [scissors, rock], [scissors, paper], [scissors, scissors]
+
+const rv2 = rockPaperScissors(3);
+console.log(rv2.length);    // should print 27
+
+const rv3 = rockPaperScissors(4);
+console.log(rv3.length);    // should print 81
+
+const rv4 = rockPaperScissors(5);
+console.log(rv4.length);    // should print 243
