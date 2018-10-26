48767d4005834c8ad2ef90cafce1fde764957a83
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-25 17:41:03 -0700

    Added new problem for DepthFirstSeach

---
+++ DepthFirstSearch/DeapthFirstSearch.js
@@ -0,0 +1,2 @@
+// Implement a function depthFirstForEach, once as a recursive function, and then again in an iterative fashion. Your functions should receive a callback and invoke that callback on each node as it is traversing in depth-first fashion. Your functions should also receive a tree node so that it can begin traversing.
+
