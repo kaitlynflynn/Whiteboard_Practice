973b80691703daa95d686760d46c447979a7986f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-25 17:55:04 -0700

    Added problem to new folder for whiteboard practice

---
+++ OneWayFlightTrip/OneWayFlight.js
@@ -0,0 +1,4 @@
+// You've booked a really cheap one-way flight. Unfortunately, that means you have tons of layovers before you reach your destination. The morning of, you woke up late and had to scramble to the airport to catch your first flight. However, in your rush, you accidentally scrambled all your flight tickets. You don't know the route of your entire trip now!
+
+// Write a function reconstructTrip to reconstruct your trip from your mass of flight tickets. Each ticket is a two-element array with the source airport as the first element and the destination airport as the second element. The ticket for your first flight has a destination but no source, and the ticket for your final flight has a source, but no destination.
+
