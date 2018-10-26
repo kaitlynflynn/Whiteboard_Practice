a5ade8d7e62830ef2194043500003f97665f4e1f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-26 16:13:47 -0700

    Created function called robotPaths & finished solving problem

--- RobotPaths/RobotPaths.js
+++ RobotPaths/RobotPaths.js
@@ -38,3 +38,32 @@ function makeBoard(n) {
     };
     return board;
   }
+
+  function robotPaths(n) {
+    let pathCounter = 0;
+    const board = makeBoard(n);
+  
+    const traversePaths = (i, j) => {
+      if (i === n - 1 && j === n - 1) {
+        pathCounter++;
+        return;
+      }
+      if (i < 0 || i >= n || j < 0 || j >= n) {
+        return;
+      }
+      if (board.hasBeenVisited(i,j)) {
+        return;
+      }
+      else {
+        board.toggle(i, j);
+        traversePaths(i, j + 1);
+        traversePaths(i + 1, j);
+        traversePaths(i, j - 1);
+        traversePaths(i - 1, j);
+        board.toggle(i, j);
+      }
+    };
+    
+    traversePaths(0, 0);
+    return pathCounter;
+  }
