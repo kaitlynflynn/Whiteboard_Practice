8d1f0a7790fb396f772f5339e59c67d5a810d89f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-25 18:00:31 -0700

    added solution for OneWayFlight problem

--- OneWayFlightTrip/OneWayFlight.js
+++ OneWayFlightTrip/OneWayFlight.js
@@ -18,3 +18,28 @@
 
 // Your function should output an array with the route of your trip. For the above example, it should look like this:
 // ['LAX', 'SFO', 'BHM', 'FLG', 'XNA', 'CID', 'SLC', 'PIT', 'ORD']
+
+// WHITEBOARD YOUR SOLUTION HERE: 
+function reconstructTrip(tickets) {
+    // use an object to associate sources and destinations 
+    const hash = {};
+    const route = Array(tickets.length - 1);
+
+    tickets.forEach(ticket => {
+        // check for the start destination of our trip
+        if (ticket[0] === null) {
+            // add it to our 'route' array as the first element
+            route[0] = ticket[1];
+        }
+        // hash each ticket with the source as key and destination as value
+        hash[ticket[0]] = ticket[1];
+    });
+
+    // loop through our object, grabbing the sources associated destination and adding it to our route
+    for (let i = 1; i < tickets.length -1; i++) {
+        route[i] = hash[route[i-1]];
+    }
+
+    return route;
+}
+
