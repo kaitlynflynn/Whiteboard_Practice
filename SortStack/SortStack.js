8fca71b58f44ccc474e9370dbaf2e7ef99477c74
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-27 21:57:12 -0700

    Finished solution for SortStack.js

--- SortStack/SortStack.js
+++ SortStack/SortStack.js
@@ -62,3 +62,28 @@ function sortStack(s) {
     return r;
 }
 
+class Stack {
+    constructor() {
+        this.storage = [];
+    }
+
+    push(item) {
+        this.storage.push(item);
+    }
+
+    pop() {
+        return this.storage.pop();
+    }
+
+    peek() {
+        return this.storage[this.storage.length-1];
+    }
+
+    isEmpty() {
+        return this.storage.length === 0;
+    }
+
+    printContents() {
+        this.storage.forEach(elem => console.log(elem));
+    }
+}
