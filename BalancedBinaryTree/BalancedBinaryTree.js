77b3d96ef7550fdd7fa4426a1bf819bbbef30c80
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-30 13:50:30 -0700

    Added some console log tests

--- BalancedBinaryTree/BalancedBinaryTree.js
+++ BalancedBinaryTree/BalancedBinaryTree.js
@@ -41,3 +41,31 @@ const checkBalanced = (rootNode) => {
     
     return (maxDepth(rootNode) - minDepth(rootNode) === 0);
   };
+
+  /* Some console.log tests */
+class BinaryTreeNode {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  
+    insertLeft(value) {
+      this.left = new BinaryTreeNode(value);
+      return this.left;
+    }
+  
+    insertRight(value) {
+      this.right = new BinaryTreeNode(value);
+      return this.right;
+    }
+  }
+  
+  const root = new BinaryTreeNode(5);
+  console.log(checkBalanced(root));   // should print true
+  
+  root.insertLeft(10);
+  console.log(checkBalanced(root));   // should print false
+  
+  root.insertRight(11);
+  console.log(checkBalanced(root));   // should print true;
