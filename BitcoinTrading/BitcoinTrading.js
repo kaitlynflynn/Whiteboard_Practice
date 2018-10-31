ee087150dd03b29ef8753f0dae9d595f3d2c9545
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-31 11:09:45 -0700

    Added my approach for this whiteboard problem

--- BitcoinTrading/BitcoinTrading.js
+++ BitcoinTrading/BitcoinTrading.js
@@ -6,3 +6,28 @@
 // findMaxProfit(bitcoinPrices);   // should return 3530, which is the max profit you can make from these prices by buying at 270 and selling at 3800
 
 //WHITEBOARD YOUR APPROACH HERE: 
+/* 
+  Our function traverses the array of prices in a greedy
+  fashion, keeping track of both the max profit we've seen
+  up to that point as well as the min price we've seen so 
+  far. On the next iteration, upate our variables if we find
+  find a higher max profit and/or a new min price.
+  In the case that we receive an array of all descending prices,
+  our function will return the least negative value, though this
+  is simply a design choice. The interviewee could choose to
+  return 0 instead if the function would return a negative value.
+*/
+
+function findMaxProfit(prices) {
+    let minPrice = prices[0];
+    let maxProfit = prices[1] - minPrice;
+  
+    for (let i = 1; i < prices.length; i++) {
+      const currentPrice = prices[i];
+  
+      maxProfit = Math.max(currentPrice - minPrice, maxProfit);
+      minPrice = Math.min(currentPrice, minPrice);
+    }
+  
+    return maxProfit;
+  }
