a497d4cba260e4590241bd3324c7a40554643aae
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-10-31 11:06:17 -0700

    Created closers and setup for loop & if statements

--- BalancedBrackets/BalancedBrackets.js
+++ BalancedBrackets/BalancedBrackets.js
@@ -29,3 +29,23 @@ function balancedBrackets(str) {
       '{': '}', 
       '[': ']'
     };
+
+    const closers = {
+        ')': true, 
+        '}': true, 
+        ']': true
+      };
+    
+      for (let i = 0; i < line.length; i++) {
+        // if we see an opener, push it onto our stack
+        if (openers[line[i]]) {
+          stack.push(line[i]);
+        } else if (closers[line[i]]) {
+          // if the closer doesn't correspond to the most
+          // recently seen unclosed opener, return false
+          if (openers[stack.pop()] !== line[i]) return false;
+        }
+      }
+    
+      return stack.length === 0;
+    }
