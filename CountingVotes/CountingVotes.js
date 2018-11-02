eaa97b2d3d3b3a36ac060863312381ec85758fc0
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-01 19:07:06 -0700

    Added new file/folder for CountingVotes Whiteboard Problem

---
+++ CountingVotes/CountingVotes.js
@@ -0,0 +1,9 @@
+// You've been hired to write the software to count the votes for a local election. Write a function countVotes that receives an array of (unique) names, each one representing a vote for that person. Your function should return the name of the winner of the election. In the case of a tie, the person whose name comes last alphabetically wins the election (a dumb rule to be sure, but the votes don't need to know).
+
+// Example:
+
+// const votes = ['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'michael'];
+
+// countVotes(votes);  // should return 'michael'
+
+//WHITEBOARD YOUR APPROACH HERE: 
