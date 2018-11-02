59dc0dc383c7d64090307a93c3277767980c986f
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-01 20:18:01 -0700

    Added if/else statement to check if guess comes after 1st word or is 1st word; returns ceilingIndex

--- FindRotationPoint/FindRotationPoint.js
+++ FindRotationPoint/FindRotationPoint.js
@@ -40,3 +40,23 @@ function findRotationPoint(words) {
     while (floorIndex < ceilingIndex) {
       // guess a point halfway between floor and ceiling
       let guessIndex = Math.floor(floorIndex + ((ceilingIndex - floorIndex) / 2));
+
+      // if guess comes after first word or is the first word
+      if (words[guessIndex] >= firstWord) {
+        // go right
+        floorIndex = guessIndex;
+      } else {
+        // go left
+        ceilingIndex = guessIndex;
+      }
+
+      // if floor and ceiling have converged
+      if (floorIndex + 1 === ceilingIndex) {
+        // between floor and ceiling is where we flipped to the beginning
+        // so ceiling is alphabetically first
+        break;
+      }
+    }
+  
+    return ceilingIndex;
+  }
