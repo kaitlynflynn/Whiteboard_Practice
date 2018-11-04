45b1ecbbc721e22b0c298762b6563464975baa6b
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-04 15:08:35 -0800

    Setup some console log tests

--- LargestStack/LargestStack.js
+++ LargestStack/LargestStack.js
@@ -90,3 +90,16 @@ class Stack {
         return this.maxValues.peek();
       }
     }
+
+/* Some console.log tests */
+const maxStack = new MaxStack();
+console.log(maxStack.getMax());   // should print `null`
+
+maxStack.push(1);
+console.log(maxStack.getMax());   // should print 1
+
+maxStack.push(100);
+console.log(maxStack.getMax());   // should print 100
+
+maxStack.pop();
+console.log(maxStack.getMax());   // should print 1
