753f7288e05f70e5e495a047ced20d9744472b2e
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-03 17:49:29 -0700

    Added new file/folder for LargestContiguousSum Whiteboard Problem

---
+++ LargestContiguousSum/LargestContiguousSum.js
@@ -0,0 +1,9 @@
+// Given an array of positive and negative integers, write a function to find the contiguous sequence with the largest sum. Return the sum.
+
+// Example
+
+// const arr = [2, 3, -8, -1, 2, 4, -2, 3];
+// console.log(largestContiguousSum(arr)); // should print 7
+
+// WHITEBOARD YOUR APPROACH HERE: 
+
