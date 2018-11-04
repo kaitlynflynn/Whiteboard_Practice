7ce2a214f4e79c7077b550672ee2e520a3612051
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-03 17:50:47 -0700

    Added some console log tests

--- LargestContiguousSum/LargestContiguousSum.js
+++ LargestContiguousSum/LargestContiguousSum.js
@@ -29,4 +29,7 @@ function largestContiguousSum(arr) {
     return maxSum;
   }
 
-  
+/* Some console.log tests */
+console.log(largestContiguousSum([5, -9, 6, -2, 3]));           // should print 7
+console.log(largestContiguousSum([1, 23, 90, 0, -9]));          // should print 114
+console.log(largestContiguousSum([2, 3, -8, -1, 2, 4, -2, 3])); // should print 7
