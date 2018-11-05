577fa9b0a2edad16ddff9972a5212fc85b9eacd0
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-05 11:54:57 -0800

    Setup some console log tests

--- MergingTwoPackages/MergingTwoPackages.js
+++ MergingTwoPackages/MergingTwoPackages.js
@@ -30,3 +30,24 @@ function getIndicesOfItemWeights(arr, limit) {
       }
       return [];
     }
+
+/* Some simple console.log tests */
+console.log(getIndicesOfItemWeights(
+    [4, 6, 10, 15, 16],
+    21
+  ));   // should print [3, 1]
+  
+  console.log(getIndicesOfItemWeights(
+    [4, 4],
+    8
+  ));   // should print [1, 0]
+  
+  console.log(getIndicesOfItemWeights(
+    [12, 6, 7, 14, 19, 3, 0, 25, 40],
+    7
+  ));   // should print [6, 2]
+  
+  console.log(getIndicesOfItemWeights(
+    [9],
+    9
+  ));   // should print []
