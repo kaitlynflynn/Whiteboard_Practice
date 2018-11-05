9b4a323c7f590d73815ed6fbb3ff7887223ae7ce
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-05 11:45:40 -0800

    Setup function spiralCopy, passing in inputMatrix; setup consts/lets

--- MatrixSpiralCopy/MatrixSpiralCopy.js
+++ MatrixSpiralCopy/MatrixSpiralCopy.js
@@ -13,3 +13,12 @@
 // output: [1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12]
 
 //WHITEBOARD YOUR APPROACH HERE: 
+function spiralCopy(inputMatrix) {
+    const numRows = inputMatrix.length;
+    const numCols = inputMatrix[0].length;
+    // keep track of where we are along each
+    // of the four sides of the matrix
+    let topRow = 0;
+    let bottomRow = numRows - 1;
+    let leftCol = 0;
+    let rightCol = numCols - 1;
