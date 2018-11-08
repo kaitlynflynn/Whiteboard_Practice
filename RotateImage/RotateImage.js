b61e45005423361bc3a9e564878b8eeee29c908b
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-11-07 16:05:00 -0800

    Added some console log tests

--- RotateImage/RotateImage.js
+++ RotateImage/RotateImage.js
@@ -56,3 +56,21 @@ function functionalRotateImage(matrix) {
     return matrix.map(m => m.reverse())[0]
                  .map((cv, i) => matrix.map((rv, j) => matrix[j][i]));
   }
+
+/* Some console.log tests */
+console.log(rotateImage([
+    [1, 2],
+    [3, 4]
+  ]));    // should print [ [ 2, 4 ], [ 1, 3 ] ]
+  
+  console.log(rotateImage([
+    [1, 1, 5, 9, 9],
+    [2, 2, 6, 0, 0],
+    [3, 3, 7, 1, 1],
+    [4, 4, 8, 2, 2],
+    [5, 5, 9, 3, 3]
+  ]));    // should print [ [ 9, 0, 1, 2, 3 ],
+          //                [ 9, 0, 1, 2, 3 ],
+          //                [ 5, 6, 7, 8, 9 ],
+          //                [ 1, 2, 3, 4, 5 ],
+          //                [ 1, 2, 3, 4, 5 ] ]
