a4f436a738f58d4e844a5cffd42d40e0265836fd
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-01-23 12:31:02 -0800

    Added practice for palindrome

--- Practice.js
+++ Practice.js
@@ -16,3 +16,30 @@ function reverseString2(str) {
 }
 console.log(reverseString2('hello harry'));
 console.log('------------');
+
+//------------------------------------------------------
+
+// Write a function to determine if a word is a palindrome.  
+
+function Palindrome(str) {
+    str =  str.toLowerCase(); //sets to lowercase
+     var str2 = str.split('').reverse().join('');
+   
+       return str === str2;
+   }
+   console.log(Palindrome('Clementine')); // return false
+   console.log(Palindrome('Racecar')); // returns true
+   
+   // another solution:
+   function Palindrome2(str) {
+   
+     str = str.toUpperCase(); //sets to uppercase
+     for (let first = 0, last = str.length -1; first < last; ++first, --last) {
+        if (str[first] !== str[last]) return false;
+     }
+     return true;
+   }
+   console.log(Palindrome2('tacocat')); // returns true
+   console.log(Palindrome2('Pop')); // returns true
+   console.log(Palindrome2('pug')); // returns false
+   console.log('------------');
